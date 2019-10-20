bf0b95483642f80d2f14c653b6237300a1c467ea
Sonu Sourav <sonusouravdx001@gmail.com>
2019-10-20 15:35:23 +0530

    Initial commit

---
+++ app/src/main/java/com/sonusourav/ideabhejo/MainActivity.java
@@ -0,0 +1,146 @@
+package com.sonusourav.ideabhejo;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String postUrl = "https://oss2019.github.io/";
+    private WebView webView;
+    private ProgressBar progressBar;
+    private float m_downX;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+       if(getSupportActionBar()!=null){
+           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+           getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+       }
+
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if (!TextUtils.isEmpty(getIntent().getStringExtra("postUrl"))) {
+            postUrl = getIntent().getStringExtra("postUrl");
+        }
+
+        initWebView();
+        renderPost();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled") private void initWebView() {
+        webView.setWebChromeClient(new MyWebChromeClient(this));
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /**
+                 * Check for the url, if the url is from same domain
+                 * open the url in the same activity as new intent
+                 * else pass the url to browser activity
+                 * */
+                if (Utils.isSameDomain(postUrl, url)) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    intent.putExtra("postUrl", url);
+                    startActivity(intent);
+                } else {
+                    // launch in-app browser i.e BrowserActivity
+                    openInAppBrowser(url);
+                }
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getPointerCount() > 1) {
+                    //Multi touch detected
+                    return true;
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // save the x
+                        m_downX = event.getX();
+                    }
+                    break;
+
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP: {
+                        // set x so that it doesn't move
+                        event.setLocation(m_downX, event.getY());
+                    }
+                    break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void renderPost() {
+        webView.loadUrl(postUrl);
+
+        // webView.loadUrl("file:///android_asset/sample.html");
+    }
+
+    private void openInAppBrowser(String url) {
+        Intent intent = new Intent(MainActivity.this, BrowserActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+    /**
+     * Initializing collapsing toolbar
+     * Will show and hide the toolbar txtPostTitle on scroll
+     */
+
+    private class MyWebChromeClient extends WebChromeClient {
+        Context context;
+
+        public MyWebChromeClient(Context context) {
+            super();
+            this.context = context;
+        }
+
+
+    }
+}
